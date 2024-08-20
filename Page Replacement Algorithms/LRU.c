fe3d836e3234c7147f006cce28797d0c73aa3607
Niranjay Ajayan <128250840+dyingpotato890@users.noreply.github.com>
2024-08-20 17:10:36 +0530

    Update LRU.c

--- Page Replacement Algorithms/LRU.c
+++ Page Replacement Algorithms/LRU.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-int lastused[3];
-int MAX_FRAMES;
 int hitCount = 0, missCount = 0;
+int MAX_FRAMES;
+int frames[100];
+int counter[100];
 
-void initialize(int frames[]) {
-    for (int i = 0; i < MAX_FRAMES; i++)
+void initialize() {
+    for (int i = 0; i < MAX_FRAMES; i++) {
         frames[i] = -1;
-
-    memset(lastused, 0, sizeof(lastused));
+        counter[i] = 0; 
+    }
 }
 
-void displayFrames(int frames[]) {
+void displayFrames() {
     for (int i = 0; i < MAX_FRAMES; i++) {
         if (frames[i] != -1)
             printf("%d ", frames[i]);
@@ -23,9 +23,23 @@ void displayFrames(int frames[]) {
     printf("\n");
 }
 
-void LRU(int frames[], int pages[], int n) {
+// Function to find the least recently used frame
+int findLRU() {
+    int max = counter[0];
+    int lru_frame = 0;
+
+    for (int i = 1; i < MAX_FRAMES; i++) {
+        if (counter[i] > max) {
+            max = counter[i];
+            lru_frame = i;
+        }
+    }
+
+    return lru_frame;
+}
+
+void LRU(int pages[], int n) {
     int page_faults = 0;
-    int next = -1;
 
     for (int i = 0; i < n; i++) {
         int page = pages[i];
@@ -34,25 +48,39 @@ void LRU(int frames[], int pages[], int n) {
         // Check if page already exists in frames
         for (int j = 0; j < MAX_FRAMES; j++) {
             if (frames[j] == page) {
-                printf("Page %d already in frame %d\n", page, j);
                 found = 1;
+                printf("page %d is already there \n",page);
                 hitCount++;
                 break;
             }
         }
 
         if (!found) {
-            missCount++;
-            next = (next + 1) % MAX_FRAMES;  // Circularly select the next frame
-            printf("Page %d loaded in frame %d\n", page, next);
-            frames[next] = page;
+            int lru_frame = findLRU();
+            printf("page %d is loaded in frame %d\n",page,lru_frame);
+            frames[lru_frame] = page;
+            counter[lru_frame] = 0;
             page_faults++;
+            missCount++;
         }
 
-        displayFrames(frames);
+        // Increment counter for all frames
+        for (int j = 0; j < MAX_FRAMES; j++) {
+            counter[j]++;
+        }
+
+        // Reset counter for the used frame
+        for (int j = 0; j < MAX_FRAMES; j++) {
+            if (frames[j] == page) {
+                counter[j] = 0;
+                break;
+            }
+        }
+
+        displayFrames();
     }
 
-    printf("\nTotal Page Faults: %d\n", page_faults);
+    printf("Total Page Faults: %d\n", page_faults);
     printf("Hit Count: %d\n", hitCount);
     printf("Miss Count: %d\n", missCount);
     printf("\nHit Ratio = %f", (float)hitCount / (float)(hitCount + missCount));
@@ -61,22 +89,19 @@ void LRU(int frames[], int pages[], int n) {
 
 int main() {
     int n;
-    int pages[20];
-
-    printf("Enter the number of page requests: ");
+    printf("Enter The Number of Pages: ");
     scanf("%d", &n);
+    int pages[n];
 
-    printf("Enter the page reference sequence: ");
+    printf("Enter The Page Reference Sequence: ");
     for (int i = 0; i < n; i++)
         scanf("%d", &pages[i]);
 
     printf("Enter The Number of Frames: ");
     scanf("%d", &MAX_FRAMES);
 
-    int frames[MAX_FRAMES];
-
-    initialize(frames);
-    LRU(frames, pages, n);
+    initialize();
+    LRU(pages, n);
 
     return 0;
 }
